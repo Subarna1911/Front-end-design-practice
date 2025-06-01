d86ff8a6152347783b7e618cd36e11e8981abc5a
subarna1911 <subarnablone09@gmail.com>
2025-06-01 12:31:58 +0530

    contact-form is created

---
+++ index.js
@@ -0,0 +1,60 @@
+
+const form         = document.getElementById("contactForm");
+const thankYOuMsg  = document.getElementById("thankYou");
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const errorElements = document.querySelectorAll('.error-msg');
+    errorElements.forEach(el=>el.style.display="none");
+
+    const firstName  = document.getElementById("firstName").value.trim();
+    const lastName   = document.getElementById("lastName").value.trim();
+    const email      = document.getElementById("email").value.trim();
+    const message    = document.getElementById("message").value.trim();
+    const consent    = document.getElementById("consent").value.trim();
+
+    let isValid = true;
+    
+    if(firstName === ""){
+        document.getElementById('name-error').textContent = 'Name is required';
+        document.getElementById('name-error').style.display = 'block';
+        isValid = false;
+    }
+  
+     if(lastName === ""){
+        document.getElementById('last-name-error').textContent = 'Last Name is required';
+        document.getElementById('last-name-error').style.display = 'block';
+        isValid = false;
+    }
+    
+     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+     if(email === '' || !emailPattern.test(email)){
+        document.getElementById('email-error').textContent = 'valid email is required';
+        document.getElementById('email-error').style.display = 'block';
+        isValid = false;
+    }
+  
+     if(message === ""){
+        document.getElementById('message-error').textContent = 'Message is required';
+        document.getElementById('message-error').style.display = 'block';
+        isValid = false;
+    }
+
+     if(consent === ""){
+        document.getElementById('consent-error').textContent = 'consent is required';
+        document.getElementById('consent-error').style.display = 'block';
+        isValid = false;
+    }
+
+
+  if(isValid){
+     setTimeout(()=>{
+        form.style.display = "none";
+        thankYOuMsg.style.display = "block";
+
+    },500);
+
+  }
+   
+});
